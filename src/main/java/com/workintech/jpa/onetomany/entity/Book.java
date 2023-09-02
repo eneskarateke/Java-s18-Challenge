9fadebbc2ffb924443df97b6305bbc2345eb3b24
eneskarateke <eneskarateke@gmail.com>
2023-09-02 18:21:11 +0300

    entities created

---
+++ src/main/java/com/workintech/jpa/onetomany/entity/Book.java
@@ -0,0 +1,28 @@
+package com.workintech.jpa.onetomany.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "book", schema="spring")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id")
+    private int id;
+
+
+    @Column(name= "name")
+    private String name;
+
+    @ManyToOne(cascade = {CascadeType.MERGE,CascadeType.DETACH,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+@ManyToOne(cascade = {CascadeType.MERGE,CascadeType.DETACH,CascadeType.PERSIST,CascadeType.REFRESH})
+@JoinColumn(name = "category_id")
+    private Category category;
+}
